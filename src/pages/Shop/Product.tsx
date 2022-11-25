2f3ba10ec7548a6ab32305e944be8693bc422406
marcin-nedza <nedza.marcin@gmail.com>
2022-11-25 19:24:05 +0100

    product page media query

--- src/pages/Shop/Product.tsx
+++ src/pages/Shop/Product.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import './index.scss'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.scss";
 
-const Product:React.FC<{id:string,name:string,price:number,photo:any}> = ({id,name,price,photo}) => {
-  const navigate= useNavigate()
-  
-  const redirect=()=>{
-    navigate(`/shop/product/${id}`)
-  }
+const Product: React.FC<{
+  id: string;
+  name: string;
+  price: number;
+  photo: any;
+}> = ({ id, name, price, photo }) => {
+  const navigate = useNavigate();
+
+  const redirect = () => {
+    navigate(`/shop/product/${id}`);
+  };
   return (
-    <div className='shop__body--product' onClick={redirect}>
+    <div className="shop__body--product" onClick={redirect}>
       <div className="shop__body--product-img --margin-bottom-2">
-        <img src={photo} alt=''/>
+        <img src={photo} alt="" />
       </div>
       <div className="shop__body--product-content">
-        <p className='shop__body--product-name --margin-bottom-1'>{name}</p>
-        <p className='shop__body--product-price'>{price} zl</p>
+        <p className="shop__body--product-name --margin-bottom-1">{name}</p>
+        <div className="shop__body--product-price">
+          <p className="part1">from </p>
+          <p className="part2">$ {price} </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
